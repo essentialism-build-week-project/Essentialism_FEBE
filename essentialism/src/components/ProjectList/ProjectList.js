f6b6e3282b730cdc33deea48668e5246a5976f48
John O'Keefe <jokeefe@weegolobal.com>
2019-04-25 19:49:01 -0400

    initial form, list, and button styles

--- essentialism/src/components/ProjectList/ProjectList.js
+++ essentialism/src/components/ProjectList/ProjectList.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Card, Heading, Text } from "rebass";
+import { Button } from '../Global.Styles'
+
 export default function ProjectList(props) {
   return (
     <div>
@@ -8,18 +10,19 @@ export default function ProjectList(props) {
           <Box width={256}>
             <Card
               p={1}
+              mb={3}
               borderRadius={2}
-              boxShadow="0 0 16px rgba(0, 0, 0, .25)"
+              boxShadow="0 0 5px rgba(0, 0, 0, .25)"
             >
               <Box px={2}>
-                <Heading as="h3">{project.name}</Heading>
-                <Text fontSize={0}>{project.description}</Text>
-                <button onClick={() => props.handleDelete(project)}>
-                  delete!
-                </button>
-                <button onClick={() => props.handleModify(project)}>
-                  modify!
-                </button>
+                <Heading as="h3" pb={3}>{project.name}</Heading>
+                <Text fontSize={0} pb={3}>{project.description}</Text>
+                <Button onClick={() => props.handleDelete(project)} mr={2}>
+                  Delete
+                </Button>
+                <Button onClick={() => props.handleModify(project)}>
+                  Modify
+                </Button>
               </Box>
             </Card>
           </Box>
