343ef2cab2ac831a3dfb65248943248ab1300f1b
iner Lofgren <iner.lofgren@gmail.com>
2019-04-25 18:25:25 -0700

    add basic drag drop functionality

--- essentialism/src/components/ValueList/ValueList.js
+++ essentialism/src/components/ValueList/ValueList.js
@@ -1,35 +1,85 @@
 import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { Box, Card, Heading, Text } from "rebass";
-import { Button } from '../Global.Styles'
+import { Button } from "../Global.Styles";
 
 export default function ValueList(props) {
   console.log(props);
 
   return (
+    // <div>
+    //   {props.values.map(value => (
+    //     <div key={value.id}>
+    //       <Box width={256}>
+    //         <Card
+    //           p={1}
+    //           mb={3}
+    //           borderRadius={2}
+    //           boxShadow="0 0 5px rgba(0, 0, 0, .25)"
+    //         >
+    //           <Box px={2}>
+    //             <Heading as="h3" pb={3}>{value.name}</Heading>
+    //             <Text fontSize={0} pb={3  }>{value.description}</Text>
+    //             <Button onClick={() => props.handleDelete(value)} mr={2 }>
+    //               delete!
+    //             </Button>
+    //             <Button onClick={() => props.handleModify(value)}>
+    //               modify!
+    //             </Button>
+    //           </Box>
+    //         </Card>
+    //       </Box>
+    //     </div>
+    //   ))}
+    // </div>
+
     <div>
-      {props.values.map(value => (
-        <div key={value.id}>
-          <Box width={256}>
-            <Card
-              p={1}
-              mb={3}
-              borderRadius={2}
-              boxShadow="0 0 5px rgba(0, 0, 0, .25)"
-            >
-              <Box px={2}>
-                <Heading as="h3" pb={3}>{value.name}</Heading>
-                <Text fontSize={0} pb={3  }>{value.description}</Text>
-                <Button onClick={() => props.handleDelete(value)} mr={2 }>
-                  delete!
-                </Button>
-                <Button onClick={() => props.handleModify(value)}>
-                  modify!
-                </Button>
-              </Box>
-            </Card>
-          </Box>
-        </div>
-      ))}
+      <DragDropContext onDragEnd={props.onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {props.values.map((value, index) => (
+                <Draggable key={value.id} draggableId={value.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <Box width={256}>
+                        <Card
+                          p={1}
+                          mb={3}
+                          borderRadius={2}
+                          boxShadow="0 0 5px rgba(0, 0, 0, .25)"
+                        >
+                          <Box px={2}>
+                            <Heading as="h3" pb={3}>
+                              {value.name}
+                            </Heading>
+                            <Text fontSize={0} pb={3}>
+                              {value.description}
+                            </Text>
+                            <Button
+                              onClick={() => props.handleDelete(value)}
+                              mr={2}
+                            >
+                              delete!
+                            </Button>
+                            <Button onClick={() => props.handleModify(value)}>
+                              modify!
+                            </Button>
+                          </Box>
+                        </Card>
+                      </Box>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
