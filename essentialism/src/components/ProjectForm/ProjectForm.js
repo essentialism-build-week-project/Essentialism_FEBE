beae3bfbabc97070825a46a02072cbab16ccc693
iner Lofgren <iner.lofgren@gmail.com>
2019-04-27 12:11:06 -0700

    added conditional h2 render, project button conditionally render text

--- essentialism/src/components/ProjectForm/ProjectForm.js
+++ essentialism/src/components/ProjectForm/ProjectForm.js
@@ -3,119 +3,10 @@ import { Box } from "rebass";
 import { Button, Input } from "../Global.Styles";
 import ProjectList from "../ProjectList/ProjectList";
 
-// a little function to help us with reordering the result
-// const reorder = (list, startIndex, endIndex) => {
-//   const result = Array.from(list);
-//   const [removed] = result.splice(startIndex, 1);
-//   result.splice(endIndex, 0, removed);
-
-//   return result;
-// };
-
 export default class ProjectForm extends Component {
-  // state = {
-  //   name: "",
-  //   description: "",
-  //   id: "",
-  //   isFiltered: false,
-  //   projects: []
-  // };
-
   componentDidMount = async () => {
     this.props.initialLoad();
-    // try {
-    //   const result = await API.graphql(graphqlOperation(listProjects));
-    //   const projects = result.data.listProjects.items;
-    //   this.setState({ projects });
-    // } catch (err) {
-    //   console.log("Error listing projects:", err);
-    // }
   };
-
-  // handleFilter = () => {
-  //   this.setState({ isFiltered: !this.state.isFiltered });
-  // };
-
-  // handleChange = e => {
-  //   const { name, value } = e.target;
-  //   this.setState({ [name]: value });
-  //   console.log("update");
-  // };
-
-  // handleModify = async project => {
-  //   this.setState(project);
-  // };
-
-  // handleDelete = async project => {
-  //   const { id } = project;
-  //   try {
-  //     const input = { id };
-  //     const result = await API.graphql(
-  //       graphqlOperation(deleteProject, { input })
-  //     );
-  //     const deletedProject = result.data.deleteProject;
-  //     const updateProjects = this.state.projects.filter(
-  //       value => value.id !== deletedProject.id
-  //     );
-  //     this.setState({ projects: updateProjects });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // onDragEnd = result => {
-  //   // dropped outside the list
-  //   if (!result.destination) {
-  //     return;
-  //   }
-
-  //   const projects = reorder(
-  //     this.state.projects,
-  //     result.source.index,
-  //     result.destination.index
-  //   );
-
-  //   this.setState({
-  //     projects
-  //   });
-  // };
-
-  // handleSubmit = async e => {
-  //   e.preventDefault();
-  //   const { name, description, projects, id } = this.state;
-  //   if (id !== "") {
-  //     const input = { id, name, description };
-  //     const result = await API.graphql(
-  //       graphqlOperation(updateProject, { input })
-  //     );
-  //     const updatedProject = result.data.updateProject;
-  //     const index = projects.findIndex(project => project.id === this.state.id);
-  //     const updatedProjects = [
-  //       ...this.state.projects.slice(0, index),
-  //       updatedProject,
-  //       ...this.state.projects.slice(index + 1)
-  //     ];
-  //     this.setState({
-  //       name: "",
-  //       description: "",
-  //       id: "",
-  //       projects: updatedProjects
-  //     });
-  //   } else {
-  //     const input = { name, description };
-  //     const result = await API.graphql(
-  //       graphqlOperation(createProject, { input })
-  //     );
-  //     const newProject = result.data.createProject;
-  //     const updatedProjects = [newProject, ...projects];
-
-  //     this.setState({
-  //       name: "",
-  //       description: "",
-  //       projects: updatedProjects
-  //     });
-  //   }
-  // };
 
   render() {
     return (
@@ -144,7 +35,7 @@ export default class ProjectForm extends Component {
             </label>
             <Box>
               <Button type="submit" bg="Black" mt={3}>
-                Add Project
+                {this.props.id === "" ? "Add Project" : "Modify Project"}
               </Button>
             </Box>
           </form>
