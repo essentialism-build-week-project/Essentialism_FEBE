a265ebb0996cefcdb8d66bd8346d19e5338b1d50
John O'Keefe <jokeefe@weegolobal.com>
2019-05-02 19:10:26 -0400

    changed orderlambda to orderLambda

--- essentialism/src/components/StripeButton/StripeButton.js
+++ essentialism/src/components/StripeButton/StripeButton.js
@@ -7,9 +7,9 @@ const stripeConfig = {
     PUB_KEY: 'pk_test_r7F5D6WDUTqIc0XLdf60UPt900ELuVpqII'
 };
 
-const handleCharge = async (token) => {
+const handleCharge = async token => {
     try {
-        const result = await API.post('orderlambda', '/charge', {
+        const result = await API.post('orderLambda', '/charge', {
             body: {
                 token,
                 charge: {
@@ -18,25 +18,25 @@ const handleCharge = async (token) => {
                     description: 'Support Development of Essentialize'
                 }
             }
-        })
-        console.log(result)
+        });
+        console.log(result);
+    } catch (err) {
+        console.log('There was an error charging credit card:', err);
     }
-    catch(err) {
-        console.log('There was an error charging credit card:', err)
-    }
-}
+};
 
 export default function StripeButton() {
     return (
         <>
-        <h3>
-            If you are happy with your results, consider supporting our work by sending us $1.00. Just click the link below.
-        </h3>
-        <StripeCheckout
-            token={handleCharge}
-            currency={stripeConfig.currency}
-            stripeKey={stripeConfig.PUB_KEY}
-        />
+            <h3>
+                If you are happy with your results, consider supporting our work
+                by sending us $1.00. Just click the link below.
+            </h3>
+            <StripeCheckout
+                token={handleCharge}
+                currency={stripeConfig.currency}
+                stripeKey={stripeConfig.PUB_KEY}
+            />
         </>
     );
 }
